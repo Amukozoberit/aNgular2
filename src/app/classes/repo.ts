cef9fa6270411e6e0837d7ffcb9bee51f8a172d5
Amukozoberit <mwasheb@gmail.com>
2021-07-26 12:16:41 +0300

    added data to clases

--- src/app/classes/repo.ts
+++ src/app/classes/repo.ts
@@ -1,2 +1,11 @@
 export class Repo {
+    name: string="";
+    html_url: string="";
+    fork: boolean =false;
+    repo_url: string ="";
+    language:string="";
+  
+    constructor(name?:string,html_url?:string,fork?:boolean,repo_url?:string,language?:string){
+        
+    }
 }
--- src/app/classes/user.ts
+++ src/app/classes/user.ts
@@ -1,2 +1,13 @@
 export class User {
+    url: string | undefined ;
+    username: string | undefined;
+    created_at: Date=new Date();
+    repo:string|undefined;
+    user_repo:string|undefined;
+  followers: number | undefined;
+  following: number | undefined;
+    constructor(url?:string,username?:string,created_at?:Date,repo?:string,followers?:number,following?:number){
+   
+      
+    }
 }
