3df2d1e543f0eda968ef02722cc71a2edc71cab7
Amukozoberit <mwasheb@gmail.com>
2021-07-26 12:30:52 +0300

    added direct content

--- src/app/directives/click-directive.directive.ts
+++ src/app/directives/click-directive.directive.ts
@@ -1,10 +1,20 @@
-import { Directive } from '@angular/core';
+import { Directive ,ElementRef,HostListener} from '@angular/core';
 
 @Directive({
   selector: '[appClickDirective]'
 })
 export class ClickDirectiveDirective {
+@HostListener("dblclick") onDoubleClick(){
+  this.textDeco("None");
+}
 
-  constructor() { }
 
+@HostListener('click')  onClick(){
+  this.textDeco("blue");
+}
+  constructor(private elem:ElementRef) { }
+private textDeco(action:string){
+  this.elem.nativeElement.style.background=action;
+
+}
 }
