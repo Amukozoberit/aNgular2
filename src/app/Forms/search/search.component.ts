5a3c92f068c89af87f88a83de6d8c7cfc82ccf8d
Amukozoberit <mwasheb@gmail.com>
2021-07-26 12:33:09 +0300

    added search.ts

--- src/app/Forms/search/search.component.ts
+++ src/app/Forms/search/search.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { DataService } from 'src/app/services/data.service';
+
+
+
 
 @Component({
   selector: 'app-search',
@@ -6,10 +11,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-
-  constructor() { }
+@Output() isChanged=new EventEmitter<string>()
+  constructor(private dataService:DataService) { }
 
   ngOnInit(): void {
   }
+  @ViewChild('myForm', {static: false}) myForm: any ='';
+search(val:any){
+  this.isChanged.emit(val);
+  this.myForm.resetForm();
+  // alert(val);
+  // this.dataService.repoRequest(val);
+  // this.dataService.quoteRequest(val);
+  
+}
 
 }
